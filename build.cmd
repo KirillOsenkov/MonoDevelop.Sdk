b8264dc517a42ff5189f650e068b81bd34504029
Kirill Osenkov <github@osenkov.com>
2018-11-20 14:52:49 -0800

    Add more files.

--- build.cmd
+++ build.cmd
@@ -7,6 +7,7 @@ xcopy C:\md1\main\build\AddIns\CSharpBinding\MonoDevelop.CSharpBinding.dll signe
 xcopy C:\md1\main\build\AddIns\DisplayBindings\SourceEditor\MonoDevelop.SourceEditor.dll signed
 xcopy C:\md1\main\build\AddIns\MonoDevelop.DotNetCore\MonoDevelop.DotNetCore.dll signed
 xcopy C:\md1\main\build\bin\mdtool.exe signed
+xcopy C:\md1\main\build\bin\Microsoft.CodeAnalysis.CSharp.dll signed
 xcopy C:\md1\main\build\bin\Microsoft.VisualStudio.CodingConventions.dll signed
 xcopy C:\md1\main\build\bin\Microsoft.VisualStudio.Composition.dll signed
 xcopy C:\md1\main\build\bin\Microsoft.VisualStudio.CoreUtility.dll signed
@@ -19,11 +20,13 @@ xcopy C:\md1\main\build\bin\Microsoft.VisualStudio.Text.Logic.dll signed
 xcopy C:\md1\main\build\bin\Microsoft.VisualStudio.Text.UI.dll signed
 xcopy C:\md1\main\build\bin\Microsoft.VisualStudio.Threading.dll signed
 xcopy C:\md1\main\build\bin\Microsoft.VisualStudio.Validation.dll signed
+xcopy C:\md1\main\build\bin\Mono.Addins.dll signed
 xcopy C:\md1\main\build\bin\MonoDevelop.Core.dll signed
 xcopy C:\md1\main\build\bin\MonoDevelop.Ide.dll signed
 xcopy C:\md1\main\build\bin\Newtonsoft.Json.dll signed
 xcopy C:\md1\main\build\bin\System.Collections.Immutable.dll signed
 xcopy C:\md1\main\build\bin\System.Reflection.Metadata.dll signed
+xcopy C:\md1\main\build\bin\Xwt.dll signed
 
 pushd signed
 
