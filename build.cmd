ab12ec6dfd981bbf04ecb29c62f1d47830005a92
Kirill Osenkov <github@osenkov.com>
2019-06-10 13:53:11 -0700

    Update to 1.0.9.
    MonoDevelop master SHA:
    9cd869463a57832e1d423f345a4e189b46b276e0

--- build.cmd
+++ build.cmd
@@ -1,34 +1,33 @@
 rmdir /s /q signed
 md signed
 
-xcopy C:\xmd\main\build\addins\CSharpBinding\MonoDevelop.CSharpBinding.AspNet.dll signed
-xcopy C:\xmd\main\build\addins\CSharpBinding\MonoDevelop.CSharpBinding.Autotools.dll signed
-xcopy C:\xmd\main\build\AddIns\CSharpBinding\MonoDevelop.CSharpBinding.dll signed
-xcopy C:\xmd\main\build\AddIns\DisplayBindings\SourceEditor\MonoDevelop.SourceEditor.dll signed
-xcopy C:\xmd\main\build\AddIns\MonoDevelop.DotNetCore\MonoDevelop.DotNetCore.dll signed
+xcopy C:\md\main\build\addins\CSharpBinding\MonoDevelop.CSharpBinding.AspNet.dll signed
+xcopy C:\md\main\build\addins\CSharpBinding\MonoDevelop.CSharpBinding.Autotools.dll signed
+xcopy C:\md\main\build\AddIns\CSharpBinding\MonoDevelop.CSharpBinding.dll signed
+xcopy C:\md\main\build\AddIns\DisplayBindings\SourceEditor\MonoDevelop.SourceEditor.dll signed
+xcopy C:\md\main\build\AddIns\MonoDevelop.DotNetCore\MonoDevelop.DotNetCore.dll signed
+xcopy C:\md\main\build\bin\mdtool.exe signed
+xcopy C:\md\main\build\bin\Microsoft.CodeAnalysis.CSharp.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.CodingConventions.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Composition.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.CoreUtility.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Language.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Language.Intellisense.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Language.StandardClassification.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Text.Data.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Text.Logic.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Text.UI.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Threading.dll signed
+xcopy C:\md\main\build\bin\Microsoft.VisualStudio.Validation.dll signed
+xcopy C:\md\main\build\bin\Mono.Addins.dll signed
+xcopy C:\md\main\build\bin\MonoDevelop.Core.dll signed
+xcopy C:\md\main\build\bin\MonoDevelop.Ide.dll signed
+xcopy C:\md\main\build\bin\Newtonsoft.Json.dll signed
+xcopy C:\md\main\build\bin\System.Collections.Immutable.dll signed
+xcopy C:\md\main\build\bin\System.Reflection.Metadata.dll signed
+xcopy C:\md\main\build\bin\Xwt.dll signed
 xcopy "C:\Program Files (x86)\GtkSharp\2.12\lib\gtk-sharp-2.0\glib-sharp.dll" signed
 xcopy "C:\Program Files (x86)\GtkSharp\2.12\lib\gtk-sharp-2.0\gtk-sharp.dll" signed
-xcopy C:\xmd\main\build\bin\mdtool.exe signed
-xcopy C:\xmd\main\build\bin\Microsoft.CodeAnalysis.CSharp.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.CodingConventions.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Composition.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.CoreUtility.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Language.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Language.Intellisense.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Language.StandardClassification.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Text.Data.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Text.Implementation.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Text.Logic.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Text.UI.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Threading.dll signed
-xcopy C:\xmd\main\build\bin\Microsoft.VisualStudio.Validation.dll signed
-xcopy C:\xmd\main\build\bin\Mono.Addins.dll signed
-xcopy C:\xmd\main\build\bin\MonoDevelop.Core.dll signed
-xcopy C:\xmd\main\build\bin\MonoDevelop.Ide.dll signed
-xcopy C:\xmd\main\build\bin\Newtonsoft.Json.dll signed
-xcopy C:\xmd\main\build\bin\System.Collections.Immutable.dll signed
-xcopy C:\xmd\main\build\bin\System.Reflection.Metadata.dll signed
-xcopy C:\xmd\main\build\bin\Xwt.dll signed
 
 pushd signed
 
