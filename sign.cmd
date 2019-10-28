5b9766714cb9985a089ab20339b6511f4bc70e86
Kirill Osenkov <github@osenkov.com>
2019-10-27 19:41:30 -0700

    Bump version and add sign.cmd.

---
+++ sign.cmd
@@ -0,0 +1,12 @@
+pushd signed
+
+sn -Ra MonoDevelop.Core.dll C:\md-addins\MonoDevelop.snk
+sn -Ra MonoDevelop.CSharpBinding.AspNet.dll C:\md-addins\MonoDevelop.snk
+sn -Ra MonoDevelop.CSharpBinding.Autotools.dll C:\md-addins\MonoDevelop.snk
+sn -Ra MonoDevelop.CSharpBinding.dll C:\md-addins\MonoDevelop.snk
+sn -Ra MonoDevelop.DotNetCore.dll C:\md-addins\MonoDevelop.snk
+sn -Ra MonoDevelop.Ide.dll C:\md-addins\MonoDevelop.snk
+sn -Ra MonoDevelop.SourceEditor.dll C:\md-addins\MonoDevelop.snk
+sn -Ra MonoDevelop.TextEditor.dll C:\md-addins\MonoDevelop.snk
+
+popd
