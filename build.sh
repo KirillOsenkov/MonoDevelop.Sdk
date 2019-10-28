95f0e1a9b7a7b370c471ae5146b50d4335082cf4
Kirill Osenkov <github@osenkov.com>
2019-10-27 19:40:56 -0700

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+mkdir out
+cp /Library/Frameworks/Mono.framework/Versions/6.6.0/lib/mono/gtk-sharp-2.0/glib-sharp.dll out
+cp /Library/Frameworks/Mono.framework/Versions/6.6.0/lib/mono/gtk-sharp-2.0/gtk-sharp.dll out
+cp ~/monodevelop/main/build/AddIns/CSharpBinding/MonoDevelop.CSharpBinding.AspNet.dll out
+cp ~/monodevelop/main/build/AddIns/CSharpBinding/MonoDevelop.CSharpBinding.Autotools.dll out
+cp ~/monodevelop/main/build/AddIns/CSharpBinding/MonoDevelop.CSharpBinding.dll out
+cp ~/monodevelop/main/build/AddIns/DisplayBindings/SourceEditor/MonoDevelop.SourceEditor.dll out
+cp ~/monodevelop/main/build/AddIns/DisplayBindings/TextEditor/MonoDevelop.TextEditor.dll out
+cp ~/monodevelop/main/build/AddIns/MonoDevelop.DotNetCore/MonoDevelop.DotNetCore.dll out
+cp ~/monodevelop/main/build/bin/Mono.Addins.dll out
+cp ~/monodevelop/main/build/bin/MonoDevelop.Core.dll out
+cp ~/monodevelop/main/build/bin/MonoDevelop.Ide.dll out
+cp ~/monodevelop/main/build/bin/Xwt.dll out
